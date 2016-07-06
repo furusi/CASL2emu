c52071053660fc3a05b9685c6c024915e9197687
furusho@w <furusityofu@gmail.com>
2016-07-06 18:38:01 +0900

    commetiiから文字列出力

--- app/src/main/java/com/example/furusho/casl2sim/Commetii.java
+++ app/src/main/java/com/example/furusho/casl2sim/Commetii.java
@@ -1,14 +1,25 @@
 package com.example.furusho.casl2sim;
 
+import android.app.Application;
+import android.content.Intent;
+import android.widget.TextView;
+
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 /**
  * Created by furusho on 2016/07/05.
  */
 public class Commetii {
+
+    @BindView(R.id.output) TextView textView;
+    Application app;
+
     int address;
 
     int gr0;
@@ -29,20 +40,29 @@ public class Commetii {
 
     final List<String> sourceCode;
 
-    public Commetii(String[] text) {
+
+
+
+    public Commetii(String[] text, Application application) {
         sourceCode = Lists.newArrayList(text);
 
         //stringから一行ずつ取り出す
         //行頭がaddaならば
         //二つの文字を足し、settextする。
+        Intent outputIntent = new Intent();
+        outputIntent.putExtra("output","fromCommet22");
+        outputIntent.setAction("com.example.furusho.casl2sim.output");
+        app = application;
+        app.getApplicationContext().sendBroadcast(outputIntent);
 
         //out 1ならば
         //gr0を出力する
     }
 
     //LAD GR2,1,GR2
-    void cm2OUT(int output,int outputLength){
+    void cm2OUT(int output,int outputLength, TextView t){
         System.out.println(output+"%d\n");
+        //t.setText("from Commet2");
     }
 
 
--- app/src/main/java/com/example/furusho/casl2sim/OutputScreen.java
+++ app/src/main/java/com/example/furusho/casl2sim/OutputScreen.java
@@ -1,5 +1,9 @@
 package com.example.furusho.casl2sim;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -14,19 +18,37 @@ public class OutputScreen extends AppCompatActivity {
     @BindView(R.id.output) TextView textView;
 
     String[] code;
+    Commetii cm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_output_screen);
         code = this.getIntent().getStringArrayExtra(new String("sourcecode"));
         ButterKnife.bind(this);
+        OutputReceiver myReceiver = new OutputReceiver();
 
-        Commetii cm = new Commetii(code);
+        IntentFilter mIF = new IntentFilter();
+        mIF.addAction("com.example.furusho.casl2sim.output");
+        registerReceiver(myReceiver, mIF);
+        cm = new Commetii(code,this.getApplication());
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        textView.setText(Joiner.on("\n").skipNulls().join(code));
+        //textView.setText(Joiner.on("\n").skipNulls().join(code));
+        cm.cm2OUT(1,1,textView);
+    }
+
+    public void updateText(String t){
+        textView.setText(t);
+    }
+
+    public class OutputReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            textView.setText(intent.getStringExtra("output"));
+        }
     }
 }
