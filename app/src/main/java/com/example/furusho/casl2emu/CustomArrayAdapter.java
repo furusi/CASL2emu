c18fd5b2604fe31903a083b5d70064bf946dbb0f
furusho <furusityofu@gmail.com>
2016-08-22 19:13:53 +0900

    行番号を表示

--- app/src/main/java/com/example/furusho/casl2emu/CustomArrayAdapter.java
+++ app/src/main/java/com/example/furusho/casl2emu/CustomArrayAdapter.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.databinding.ObservableArrayList;
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Observable;
 
 /**
@@ -22,6 +24,7 @@ public class CustomArrayAdapter extends ArrayAdapter<String> {
     int resourceId;
     Typeface tf;
     ArrayList<String> arry;
+    LayoutInflater layoutInflater =null;
 
 
     public CustomArrayAdapter(Context context, int resource, ArrayList<String> arry, Typeface tf) {
@@ -30,14 +33,20 @@ public class CustomArrayAdapter extends ArrayAdapter<String> {
         this.context=context;
         this.arry = arry;
         this.tf =Typeface.create(tf,Typeface.NORMAL);
+        this.layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
+
 
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view= super.getView(position, convertView, parent);
-        TextView textView = (TextView)view;
-        textView.setTypeface(tf);
-        return textView;
+        convertView= layoutInflater.inflate(R.layout.column_row,parent,false);
+        //View view= super.getView(position, convertView, parent);
+        //1TextView textView = (TextView)convertView.findViewById(R.id.rowbody);
+        ((TextView)convertView.findViewById(R.id.rowbody)).setTypeface(tf);
+        ((TextView)convertView.findViewById(R.id.rowbody)).setText(arry.get(position));
+        ((TextView)convertView.findViewById(R.id.rownumber)).setText(String.format(Locale.US,"0x%04X",position*4&0xFFFF));
+
+        return convertView;
     }
 }
