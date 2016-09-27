4f9bf24e75e7003842ea6d163814c3bd21ab7f41
furusho@w <furusityofu@gmail.com>
2016-09-27 11:49:06 +0900

    ログイン情報の保持

--- app/src/main/java/jp/ac/fukuoka_u/tl/casl2emu/LoginActivity.java
+++ app/src/main/java/jp/ac/fukuoka_u/tl/casl2emu/LoginActivity.java
@@ -4,7 +4,9 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -68,10 +70,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        if(preferences.contains("userid")){
+            mEmailView.setText(preferences.getString("userid",""));
+        }
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
+        if(preferences.contains("password")){
+            mPasswordView.setText(preferences.getString("password",""));
+        }
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
@@ -340,6 +349,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
             if (success) {
                 finish();
+                final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("userid",mEmail.toUpperCase());
+                editor.putString("password",mPassword);
+                editor.commit();
                 startActivity(new Intent(getApplicationContext(),ContextDisplayScreen.class));
             } else {
                 //mPasswordView.setError(getString(R.string.error_incorrect_password));
